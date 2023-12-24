c8bdf4cfd300045a947d7218c805bc7ca7968488
Preston Jenkins <preston@eltonjenkinslaw.com>
2023-12-24 15:45:44 -0600

    Update package.json and server.js, add new views and partials

---
+++ src/server.js
@@ -0,0 +1,48 @@
+/**
+ * Initializes an Express server, configures middleware,
+ * sets up routes and starts listening on the specified port.
+ */
+const express = require("express");
+const session = require("express-session");
+const path = require("path");
+const fs = require('fs');
+const bodyParser = require("body-parser");
+const searchRoutes = require("./routes/search");
+require("dotenv").config();
+
+// Initialize express app
+const app = express();
+
+// Load environment variables from .env file
+const { PORT, NODE_SESSION_SECRET } = process.env;
+
+// Configure session middleware
+app.use(
+	session({
+		secret: NODE_SESSION_SECRET,
+		resave: false,
+		saveUninitialized: true,
+	})
+);
+
+// Parse incoming request bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set view engine and views directory
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views/pages"));
+
+// Define root route
+app.get("/", (req, res) => {
+	const counties = JSON.parse(fs.readFileSync(path.join(__dirname, 'data/counties.json'), 'utf8'));
+	res.render("index", { counties });
+});
+
+// Use search routes
+app.use(searchRoutes);
+
+// Start server
+app.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}`);
+});
