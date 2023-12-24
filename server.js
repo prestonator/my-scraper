c77aec672401a9be1a223920da0c26f2b13f958b
Preston Jenkins <preston@eltonjenkinslaw.com>
2023-12-24 13:50:35 -0600

    Refactor server.js file

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
-const express = require('express');
-const session = require('express-session');
-const path = require('path');
-const bodyParser = require('body-parser');
-const searchRoutes = require('./routes/search');
+/**
+ * Initializes an Express server, configures middleware,
+ * sets up routes and starts listening on the specified port.
+ */
+const express = require("express");
+const session = require("express-session");
+const path = require("path");
+const bodyParser = require("body-parser");
+const searchRoutes = require("./routes/search");
 
 // Initialize express app
 const app = express();
 const port = 5670;
 
 // Configure session middleware
-app.use(session({
-    secret: 'edison',
-    resave: false,
-    saveUninitialized: true
-}));
+app.use(
+	session({
+		secret: "edison",
+		resave: false,
+		saveUninitialized: true,
+	})
+);
 
 // Parse URL-encoded bodies (as sent by HTML forms)
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -25,15 +31,15 @@ app.use(bodyParser.json());
 //app.use(express.static('public'));
 
 // Set the view engine to ejs
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Set the directory where the views will be loaded from
-app.set('views', path.join(__dirname, 'views/pages'));
+app.set("views", path.join(__dirname, "views/pages"));
 
 // Define a route for the root of the app
-app.get('/', (req, res) => {
-    // Render the index.ejs file
-    res.render('index');
+app.get("/", (req, res) => {
+	// Render the index.ejs file
+	res.render("index");
 });
 
 // Use the search route
@@ -41,5 +47,5 @@ app.use(searchRoutes);
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
+	console.log(`Server listening at http://localhost:${port}`);
 });
